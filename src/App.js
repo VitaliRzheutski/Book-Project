a4bd1d2bc0443a2caf4f411b5fc2f653472f6e8e
Vitali Rzeutski <vitalrzheutsky@gmail.com>
2022-04-06 15:26:12 -0400

    create func to fetch data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,32 @@ import axios from "axios";
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [books, setBooks] = useState({ items: [] });
-  return <h1> Hello </h1>;
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  let URL_API = `https://www.googleapis.com/books/v1/volumes`;
+
+  const getAllBooks = async () => {
+    const result = await axios.get(`${URL_API}?q=${searchTerm}`);
+  };
+
+  return (
+    <section>
+      <form>
+        <label>
+          <span>Search for books</span>
+          <input
+            type="search"
+            placeholder="Search for books..."
+            value={searchTerm}
+            onChange={handleChange}
+          />
+          <button type="submit">Search</button>
+        </label>
+      </form>
+    </section>
+  );
 }
 
 export default App;
