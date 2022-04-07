7751f8202d13fcb25553d68c0ea9c220b812b453
Vitali Rzeutski <vitalrzheutsky@gmail.com>
2022-04-07 12:22:30 -0400

    create filter books for sale

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import axios from "axios";
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [books, setBooks] = useState({ items: [] });
+  const [isForSale, setForSale] = useState(false);
 
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
   };
-  let URL_API = `https://www.googleapis.com/books/v1/volumes`;
+  let URL_API = "https://www.googleapis.com/books/v1/volumes";
 
   const getAllBooks = async () => {
     const result = await axios.get(`${URL_API}?q=${searchTerm}`);
@@ -22,7 +23,50 @@ function App() {
   };
 
   let booksArr = books.items;
-  console.log("books:", booksArr);
+
+  let booksForSale = booksArr.filter(
+    (book) => book.saleInfo.saleability === "FOR_SALE"
+  );
+  console.log("isForSaleBefore:", isForSale);
+
+  const renderBooks = (books) => {
+    return books.map((book, id) => {
+      const bookPrice = book.saleInfo.listPrice
+        ? book.saleInfo.listPrice.amount + book.saleInfo.listPrice.currencyCode
+        : null;
+      const pages = book.volumeInfo.pageCount;
+      const tittle = book.volumeInfo.title;
+      const publishedDate = book.volumeInfo.publishedDate;
+      // let categories = book.volumeInfo.categories;
+      // console.log("categories:", categories);
+
+      return (
+        <div key={id}>
+          <img
+            alt={book.volumeInfo.title}
+            src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
+          />
+          <div>
+            <h3>{tittle}</h3>
+            <p>Price: {bookPrice}</p>
+            <p>Pages: {pages}</p>
+            {/* <div> */}
+            {/* {categories.map((category) => {
+                return <p>Categories: {category}</p>;
+              })}
+            </div> */}
+
+            <p>Published:{publishedDate}</p>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  // console.log("booksForSale:", booksForSale);
+  // const filteredBooksByPricesLowToHigh = booksForSale.sort((book1, book2) => {
+  //   return book1.saleInfo.listPrice.amount - book2.saleInfo.listPrice.amount;
+  // });
   return (
     <section>
       <form onSubmit={handleSubmit}>
@@ -37,39 +81,16 @@ function App() {
           <button type="submit">Search</button>
         </label>
       </form>
-      <ul>
-        {booksArr.map((book, id) => {
-          console.log("single book", book.saleInfo);
-          const bookPrice = book.saleInfo.listPrice
-            ? book.saleInfo.listPrice.amount +
-              book.saleInfo.listPrice.currencyCode
-            : null;
-          const pages = book.volumeInfo.pageCount;
-          const tittle = book.volumeInfo.title;
-          const publishedDate = book.volumeInfo.publishedDate;
-          const categories = book.volumeInfo.categories;
-          console.log("categories:", categories);
-          return (
-            <div key={id}>
-              <img
-                alt={book.volumeInfo.title}
-                src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
-              />
-              <div>
-                <h3>{tittle}</h3>
-                <p>Price: {bookPrice}</p>
-                <p>Pages: {pages}</p>
-                <p>
-                  {categories.map((category) => {
-                    return <p>Categories: {category}</p>;
-                  })}
-                </p>
-                <p>Published:{publishedDate}</p>
-              </div>
-            </div>
-          );
-        })}
-      </ul>
+
+      <button type="submit" onClick={() => setForSale(true)}>
+        All books
+      </button>
+
+      <button type="submit" onClick={() => setForSale(true)}>
+        Books for sale
+      </button>
+
+      <ul>{isForSale ? renderBooks(booksForSale) : renderBooks(booksArr)}</ul>
     </section>
   );
 }
