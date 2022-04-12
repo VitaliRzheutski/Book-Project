7b29f5d0810697bf3e12839dcfd4321c0844c0b5
Vitali Rzeutski <vitalrzheutsky@gmail.com>
2022-04-12 12:04:24 -0400

    create a card for books

--- src/AllBooks.js
+++ src/AllBooks.js
@@ -6,6 +6,10 @@ import {
   InputLabel,
   Select,
   MenuItem,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
 } from "@mui/material";
 import FilterByCategory from "./FilterByCategory";
 
@@ -66,124 +70,157 @@ const AllBooks = (props) => {
   console.log("obkj", categoriesObj);
 
   return (
-    <div>
-      <Button
-        type="input"
-        variant="contained"
-        size="small"
-        onClick={() => setForSale(false)}
-      >
-        All books
-      </Button>
-
-      <Button
-        type="submit"
-        variant="contained"
-        size="small"
-        onClick={() => setForSale(true)}
-      >
-        Books for sale
-      </Button>
-
-      <Button
-        type="submit"
-        variant="contained"
-        size="small"
-        onClick={() => setFilteredByCategory(true)}
-      >
-        Filter by category{" "}
-      </Button>
-
-      <FormControl sx={{ minWidth: 150 }}>
-        <InputLabel id="demo-simple-select-label">Filter by Page</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          label="Filter by pages"
-          value={selectedOption}
-          onChange={handleOptionChange}
+    <div className="wrapper">
+      <div className="buttons">
+        <Button
+          type="input"
+          variant="contained"
+          size="small"
+          onClick={() => setForSale(false)}
         >
-          <MenuItem
-            value="All"
-            checked={selectedOption === "All"}
-            onChange={handleOptionChange}
-          >
-            All
-          </MenuItem>
+          All books
+        </Button>
 
-          <MenuItem
-            value="0-100 pages"
-            checked={selectedOption === "0-100 pages"}
-            onChange={handleOptionChange}
-          >
-            0-100 pages
-          </MenuItem>
-          <MenuItem
-            value="101-200 pages"
-            checked={selectedOption === "101-200 pages"}
-            onChange={handleOptionChange}
-          >
-            101-200 pages
-          </MenuItem>
-          <MenuItem
-            value="201-300 pages"
-            checked={selectedOption === "201-300 pages"}
-            onChange={handleOptionChange}
-          >
-            201-300 pages
-          </MenuItem>
-          <MenuItem
-            value="301-500 pages"
-            checked={selectedOption === "301-500 pages"}
-            onChange={handleOptionChange}
-          >
-            301-500 pages
-          </MenuItem>
-        </Select>
-      </FormControl>
+        <Button
+          type="submit"
+          variant="contained"
+          size="small"
+          onClick={() => setForSale(true)}
+        >
+          Books for sale
+        </Button>
 
-      <ul>
-        {isForSale ? (
-          <BooksForSale booksForSale={booksForSale} />
-        ) : isFilteredByCategory ? (
-          <FilterByCategory categoriesObj={categoriesObj} />
-        ) : (
-          allBooks.map((book, id) => {
-            const bookPrice = book.saleInfo.listPrice
-              ? book.saleInfo.listPrice.amount +
-                book.saleInfo.listPrice.currencyCode
-              : null;
-            const pages = book.volumeInfo.pageCount;
-            const tittle = book.volumeInfo.title;
-            const publishedDate = book.volumeInfo.publishedDate;
-            let categories = book.volumeInfo.categories;
+        <Button
+          type="submit"
+          variant="contained"
+          size="small"
+          onClick={() => setFilteredByCategory(true)}
+        >
+          Filter by category{" "}
+        </Button>
+      </div>
 
-            return (
-              <div key={book.id}>
-                <img
-                  alt={book.volumeInfo.title}
-                  src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
-                />
-                <div>
-                  <h3>{tittle}</h3>
-                  {bookPrice ? <p>Price: {bookPrice}</p> : null}
-                  <p>Pages: {pages}</p>
-                  <div></div>
-                  <p>Published:{publishedDate}</p>
+      <div>
+        <FormControl sx={{ minWidth: 150 }}>
+          <InputLabel id="demo-simple-select-label">Filter by Page</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            label="Filter by pages"
+            value={selectedOption}
+            onChange={handleOptionChange}
+          >
+            <MenuItem
+              value="All"
+              checked={selectedOption === "All"}
+              onChange={handleOptionChange}
+            >
+              All
+            </MenuItem>
 
-                  <div>
-                    {categories
-                      ? categories.map((category) => {
-                          return <p key={id}>Categories: {category}</p>;
-                        })
-                      : null}
+            <MenuItem
+              value="0-100 pages"
+              checked={selectedOption === "0-100 pages"}
+              onChange={handleOptionChange}
+            >
+              0-100 pages
+            </MenuItem>
+            <MenuItem
+              value="101-200 pages"
+              checked={selectedOption === "101-200 pages"}
+              onChange={handleOptionChange}
+            >
+              101-200 pages
+            </MenuItem>
+            <MenuItem
+              value="201-300 pages"
+              checked={selectedOption === "201-300 pages"}
+              onChange={handleOptionChange}
+            >
+              201-300 pages
+            </MenuItem>
+            <MenuItem
+              value="301-500 pages"
+              checked={selectedOption === "301-500 pages"}
+              onChange={handleOptionChange}
+            >
+              301-500 pages
+            </MenuItem>
+          </Select>
+        </FormControl>
+
+        <div>
+          <ul className="allBooks">
+            {isForSale ? (
+              <BooksForSale booksForSale={booksForSale} />
+            ) : isFilteredByCategory ? (
+              <FilterByCategory categoriesObj={categoriesObj} />
+            ) : (
+              allBooks.map((book, id) => {
+                const bookPrice = book.saleInfo.listPrice
+                  ? book.saleInfo.listPrice.amount +
+                    book.saleInfo.listPrice.currencyCode
+                  : null;
+                const pages = book.volumeInfo.pageCount;
+                const tittle = book.volumeInfo.title;
+                const publishedDate = book.volumeInfo.publishedDate;
+                let categories = book.volumeInfo.categories;
+
+                return (
+                  // <div key={book.id}>
+                  //   <img
+                  //     alt={book.volumeInfo.title}
+                  //     src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
+                  //   />
+                  //   <div>
+                  //     <h3>{tittle}</h3>
+                  //     {bookPrice ? <p>Price: {bookPrice}</p> : null}
+                  //     <p>Pages: {pages}</p>
+                  //     <div></div>
+                  //     <p>Published:{publishedDate}</p>
+
+                  // <div>
+                  //   {categories
+                  //     ? categories.map((category) => {
+                  //         return <p key={id}>Categories: {category}</p>;
+                  //       })
+                  //     : null}
+                  // </div>
+                  //   </div>
+                  // </div>
+
+                  <div container className="containerForBooks">
+                    <Card sx={{ maxWidth: 345 }} className="singleBook">
+                      <CardMedia
+                        component="img"
+                        height="230"
+                        src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`}
+                        alt={book.volumeInfo.title}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {book.volumeInfo.title}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          {bookPrice ? <p>Price: {bookPrice}</p> : null}
+                          <p>Pages: {pages}</p>
+                          <p key={id}>Categories: {categories}</p>
+                          <p>Published:{publishedDate}</p>
+                          {categories
+                            ? categories.map((category) => {
+                                return <p key={id}>Categories: {category}</p>;
+                              })
+                            : null}
+                        </Typography>
+                      </CardContent>
+                    </Card>
                   </div>
-                </div>
-              </div>
-            );
-          })
-        )}
-      </ul>
+                );
+              })
+            )}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
